a5749d74f8b944be882809e9021713ad8e59b500
utkarsh <agarwal.ut76@gmail.com>
2019-05-04 19:53:32 +0530

    status bar functionality

---
+++ ads.js
@@ -0,0 +1,13 @@
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width += 0.1; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
