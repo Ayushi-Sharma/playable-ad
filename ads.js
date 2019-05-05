8a8bbd97b59179234ee0d987858e5a07f3dd803f
Ayushi-Sharma <ayushi.kp@gmail.com>
2019-05-05 16:45:23 +0530

    Structuring of Main Page

--- ads.js
+++ ads.js
@@ -1,13 +1,15 @@
 function move() {
-  var elem = document.getElementById("myBar");   
+  var elem = document.getElementById('myBar');
   var width = 0;
   var id = setInterval(frame, 10);
   function frame() {
     if (width >= 100) {
       clearInterval(id);
     } else {
-      width += 0.1; 
-      elem.style.width = width + '%'; 
+      width += 0.5;
+      elem.style.width = width + '%';
     }
   }
 }
+
+move();
