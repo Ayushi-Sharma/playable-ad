59f0f931ab6fc010e3f8f17fd93f4c1540145114
Ayushi-Sharma <ayushi.kp@gmail.com>
2019-05-07 11:35:37 +0530

    Removing overflow of Loader and adding images for next screen

--- ads.js
+++ ads.js
@@ -1,15 +1,15 @@
-function move() {
-  var elem = document.getElementById('myBar');
-  var width = 0;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width += 0.5;
-      elem.style.width = width + '%';
-    }
-  }
-}
+// function move() {
+//   var elem = document.getElementById('myBar');
+//   var width = 0;
+//   var id = setInterval(frame, 10);
+//   function frame() {
+//     if (width >= 100) {
+//       clearInterval(id);
+//     } else {
+//       width += 0.5;
+//       elem.style.width = width + '%';
+//     }
+//   }
+// }
 
-move();
+// move();
