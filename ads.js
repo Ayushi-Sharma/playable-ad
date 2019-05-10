bb699d4c01661d216efe6e202e35e8d05a9c1699
utkarsh <agarwal.ut76@gmail.com>
2019-05-10 23:06:21 +0530

    second page ingredients add

--- ads.js
+++ ads.js
@@ -9,9 +9,14 @@ function gotoScene2() {
   document.querySelector('#scene2').style.display = 'flex';
   document.querySelector('#download-section').style.display = 'none';
 
-  document.getElementById('hand').addClass('animated bounceInLeft');
-  document.getElementById('plate').addClass('animated bounceInUp');
-  document.getElementById('dough').style.display='block';
+  // document.getElementById('hand').addClass('animated bounceInLeft');
+  // document.getElementById('plate').addClass('animated bounceInUp');
+  document.getElementsByClassName('dough')[0].className += " dough-add";
+  document.getElementsByClassName('bacon')[0].className += " bacon-add";
+  document.getElementsByClassName('capsicum')[0].className += " capsicum-add";
+  document.getElementsByClassName('mushroom')[0].className += " mushroom-add";
+  document.getElementsByClassName('cheese')[0].className += " cheese-add";
+  document.getElementsByClassName('sause')[0].className += " sause-add";
 }
 // document.querySelector('#main').style.display = 'none';
 document.querySelector('#scene2').style.display = 'none';
