174ae3816c13c04f6d9590a720fc43080c48b05e
Ayushi-Sharma <ayushi.kp@gmail.com>
2019-05-12 19:13:56 +0530

    Adding css for iPhone X

--- ads.js
+++ ads.js
@@ -11,26 +11,26 @@ function gotoScene2() {
 }
 
 function add_sause() {
-  document.getElementsByClassName('sause')[0].className += " ingredient-add";
+  document.getElementsByClassName('sause')[0].className += ' ingredient-add';
 }
 
 function add_cheese() {
-  document.getElementsByClassName('cheese')[0].className += " ingredient-add";
+  document.getElementsByClassName('cheese')[0].className += ' ingredient-add';
 }
 
 function add_bacon() {
-  document.getElementsByClassName('bacon')[0].className += " ingredient-add";
+  document.getElementsByClassName('bacon')[0].className += ' ingredient-add';
 }
 
 function add_capsicum() {
-  document.getElementsByClassName('capsicum')[0].className += " ingredient-add";
+  document.getElementsByClassName('capsicum')[0].className += ' ingredient-add';
 }
 
 function add_mushroom() {
-  document.getElementsByClassName('mushroom')[0].className += " ingredient-add";
+  document.getElementsByClassName('mushroom')[0].className += ' ingredient-add';
 }
 
-// document.querySelector('#main').style.display = 'none';
-document.querySelector('#scene2').style.display = 'none';
+document.querySelector('#main').style.display = 'none';
+// document.querySelector('#scene2').style.display = 'none';
 setTimeout(gotoScene2, 0000);
 // setTimeout(gotoScene3, 6000);
