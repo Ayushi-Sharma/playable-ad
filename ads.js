430ae84d5cf52ff2abafe8e57f67f2327c80d8f1
Ayushi-Sharma <ayushi.kp@gmail.com>
2019-05-13 23:02:48 +0530

    Adding character animation

--- ads.js
+++ ads.js
@@ -2,7 +2,8 @@ var isCapcicum = false,
   isMushroom = false,
   isBacon = false,
   isSauce = false,
-  isCheese = false;
+  isCheese = false,
+  isTutorial = true;
 
 function initialize() {
   document.querySelector('#main').style.display = 'flex';
@@ -31,42 +32,66 @@ function add_sause() {
 function add_cheese() {
   document.getElementsByClassName('cheese')[0].className += ' ingredient-add';
   document.querySelector('#ingredient_audio').play();
-  isCheese = false;
+  isCheese = true;
 }
 
 function add_bacon() {
   document.getElementsByClassName('bacon')[0].className += ' ingredient-add';
   document.querySelector('#ingredient_audio').play();
-  isBacon = false;
+  isBacon = true;
 }
 
 function add_capsicum() {
   document.getElementsByClassName('capsicum')[0].className += ' ingredient-add';
   document.querySelector('#ingredient_audio').play();
-  isCapcicum = false;
+  isCapcicum = true;
 }
 
 function add_mushroom() {
   document.getElementsByClassName('mushroom')[0].className += ' ingredient-add';
   document.querySelector('#ingredient_audio').play();
-  isMushroom = false;
+  isMushroom = true;
 }
 
 function end_tutorial() {
-  document.querySelector('.hand').style.display = 'none';
+  if (isTutorial) {
+    animate_character();
+    document.querySelector('.hand').style.display = 'none';
+    document.querySelector('#banner').style.display = 'none';
+    isTutorial = false;
+  }
 }
 
 function tutorial_sound() {
   document.querySelector('#tutorial_sound').style.allow = 'autoplay';
 }
 
+function goToDownloadLink() {
+  document.getElementById('dl-link').click();
+}
+
+function animate_character() {
+  document.querySelector('#banner_gif').style.display = 'flex';
+  document.querySelector('#banner_still_character').style.display = 'none';
+  setTimeout(() => {
+    document.querySelector('#banner_gif').style.display = 'none';
+    document.querySelector('#banner_still_character').style.display = 'flex';
+  }, 1000);
+}
+
+function end_game() {
+  if (isCapcicum && isMushroom && isBacon && isSauce && isCheese) {
+    animate_character();
+    setTimeout(gotoScene3, 1500);
+  }
+}
+
 document.querySelector('#main').style.display = 'none';
 document.querySelector('#scene2').style.display = 'none';
 document.querySelector('#download-section').style.display = 'none';
+document.querySelector('#banner_gif').style.display = 'none';
+document.querySelector('#banner_still_character').style.display = 'none';
+
 setTimeout(initialize, 500);
 setTimeout(gotoScene2, 4500);
 // setTimeout(tutorial_sound, 4000);
-
-if (isCapcicum && isMushroom && isBacon && isSauce && isCheese) {
-  gotoScene3();
-}
